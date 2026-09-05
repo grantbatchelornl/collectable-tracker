b6b3a2631c423da1c587b608d4c7678cb4af2fd1
grantbatchelornl <grant378899@outlook.com>
2026-09-05 14:37:21 +0000

    Remove dead binder code

--- src/pages/PublicProfile.jsx
+++ src/pages/PublicProfile.jsx
@@ -11,7 +11,6 @@ import { getInitials } from '@/lib/social';
 import AchievementBadges from '@/components/AchievementBadges';
 import ReputationBadge from '@/components/trade/ReputationBadge';
 import ShowcaseCollection from '@/components/ShowcaseCollection';
-import BinderShowcase from '@/components/binder/BinderShowcase';
 import FoundingBadge from '@/components/FoundingBadge';
 import { ArrowLeft, MessageCircle, ArrowLeftRight, Package, Loader2, DollarSign, Ban, Flag, X, BadgeCheck } from 'lucide-react';
 import {
@@ -140,7 +139,6 @@ export default function PublicProfile() {
 
       <ShowcaseCollection userId={userId} isOwnProfile={false} editable={false} />
 
-      <BinderShowcase userId={userId} />
 
       <ReputationBadge profile={profile} />
 
